8996246470253144b9082b87a8f1c94a37053e96
AckeyChan <3557985161@qq.com>
2026-09-16 21:11:07 +0800

    edition2

--- out/build/GCC 16.2.1 x86_64-redhat-linux/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ackey/文档/robomaster/projects/test
-# Build directory: /home/ackey/文档/robomaster/projects/test/out/build/GCC 16.2.1 x86_64-redhat-linux
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
