93c9a3a79e4b158ba51ecaeb71ebdb659b4023eb
AckeyChan <3557985161@qq.com>
2026-09-16 21:18:34 +0800

    ver3

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ackey/文档/robomaster/projects/test
+# Build directory: /home/ackey/文档/robomaster/projects/test/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
